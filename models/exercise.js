69a13284b10272fe43ba27743a1d468ce3ca36b1
ncougs <ncougan@gmail.com>
2021-09-24 11:59:41 +1000

    adding models

---
+++ models/exercise.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  type: String,
+  name: String,
+  weight: Number,
+  sets: Number,
+  Reps: Number,
+  Duration: Number,
+  Distance: Number
+});
+
+const Exercise = mongoose.model('exercise', ExerciseSchema);
+
+module.exports = Exercise;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Workout = require('./workout');
+const Exercise = require('./exercise');
+
+module.exports = { Workout, Exercise };
+  
